1e1506aed8623457abf112985c9dac8f6c7dfb0a
clément4444 <clement.fiquet0@gmail.com>
2025-02-10 16:17:38 +0100

    bug page home

--- src/pages/utilisateur/home/Home.tsx
+++ src/pages/utilisateur/home/Home.tsx
@@ -1,7 +1,7 @@
 import Footer from "../../../components/commun/footer/Footer";
 import HomeHeader from "../../../components/utilisateur/home/homeHeader/HomeHeader";
 import HomeHero from "../../../components/utilisateur/home/homeHero/HomeHero";
-import style from "./Home.module.css";
+import style from "./home.module.css";
 
 const Home = () => {
   return (
